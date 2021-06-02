79fea14c7ea43ba12a3aa3cc146f2aef3a067b80
ismaelviac <59666577+ismaelviac@users.noreply.github.com>
2021-06-02 22:18:30 +0200

    added blips
    added blips

--- client/main.lua
+++ client/main.lua
@@ -13,7 +13,26 @@ local Keys = {
 
 local CurrentActionMsg          = ''
 
+local blips = {
+    
+     {title="News", colour=20, id=205, x = -594.46691894531, y = -930.03179931641, z = 23.85424},
 
+  }
+      
+Citizen.CreateThread(function()
+
+    for _, info in pairs(blips) do
+      info.blip = AddBlipForCoord(info.x, info.y, info.z)
+      SetBlipSprite(info.blip, info.id)
+      SetBlipDisplay(info.blip, 4)
+      SetBlipScale(info.blip, 1.0)
+      SetBlipColour(info.blip, info.colour)
+      SetBlipAsShortRange(info.blip, true)
+	  BeginTextCommandSetBlipName("STRING")
+      AddTextComponentString(info.title)
+      EndTextCommandSetBlipName(info.blip)
+    end
+end)
 
 RegisterNetEvent("AdvertError")
 AddEventHandler("AdvertError", function(inputText)
