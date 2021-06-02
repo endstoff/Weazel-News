7fba2113d93c97048077c3cc1250b14d40dec3f0
endstoff <63933131+endstoff@users.noreply.github.com>
2021-06-02 19:34:39 +0200

    v2.0
    missing spacebar

--- locales/en.lua
+++ locales/en.lua
@@ -4,6 +4,6 @@ Locales['en'] = {
   ['error_occured'] = '~r~an error has occurred~s~',
   ['cooldown'] = 'in the last 30 minutes advertising has already been placed',
   ['DisplayAdvert'] = 'an ~g~advertisement~s~ has been placed!',
-  ['AdvertPayed'] = '~s~payed for ~y~Advertising',
+  ['AdvertPayed'] = '~s~ payed for ~y~Advertising',
   ['not_enough'] = 'you don`t have enough money to turn on ~y~advertising'
 }
