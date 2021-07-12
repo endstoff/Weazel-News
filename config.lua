0c02ddf4fa8bd8282a11695d846e62b61030d739
endstoff <63933131+endstoff@users.noreply.github.com>
2021-07-12 15:28:28 +0200

    Update config.lua

--- config.lua
+++ config.lua
@@ -1,23 +1,18 @@
 Config              = {}
+
 -- Discord Webhook
 Config.Webhook      = ""
--- Username
 Config.Username     = "Weazel News"
--- Logo
 Config.Logo         = "https://i.imgur.com/kbwaL85.jpg"
--- Author Icon
 Config.AuthorIcon   = "https://i.imgur.com/VYeiQRL.png"
--- Footertext
 Config.FooterText   = "Werbung Geschalten am"	
--- FooterIcon
 Config.FooterIcon   = "https://i.imgur.com/kbwaL85.jpg"
--- Color
 Config.Color        = 0xF23A3A -- Results in: 0xHEX_COLOR
+
 -- locale (available: en, de & es)
 Config.Locale       = 'de'
--- How much the Advert should cost players
+
 Config.AdvertCosts  = 150
 -- Coolodown for each advert in seconds (standard 30 Minutes)
 Config.Cooldown     = 1800
--- Max characters
 Config.Characters   = 150
