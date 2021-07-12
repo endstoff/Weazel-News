4dcb4501e7c2feb743539fa3b3a992ae2c237f65
endstoff <63933131+endstoff@users.noreply.github.com>
2021-07-12 18:35:53 +0200

    Update en.lua

--- locales/en.lua
+++ locales/en.lua
@@ -1,9 +1,10 @@
 Locales['en'] = {
 
-  ['advertise'] = 'press ~INPUT_CONTEXT~ to turn on ~y~advertising~s~',
-  ['error_occured'] = '~r~an error has occurred~s~',
-  ['cooldown'] = 'in the last 30 minutes advertising has already been placed',
-  ['DisplayAdvert'] = 'an ~g~advertisement~s~ has been placed!',
-  ['AdvertPayed'] = '~s~ payed for ~y~Advertising',
-  ['not_enough'] = 'you don`t have enough money to turn on ~y~advertising'
+    ['blip'] = 'Weazel News',
+    ['advertise'] = 'press ~INPUT_CONTEXT~ to turn on ~y~advertising~s~',
+    ['error_occured'] = '~r~an error has occurred~s~',
+    ['cooldown'] = 'in the last 30 minutes advertising has already been placed',
+    ['DisplayAdvert'] = 'an ~g~advertisement~s~ has been placed!',
+    ['AdvertPayed'] = '~s~payed for ~y~Advertising',
+    ['not_enough'] = 'you don`t have enough money to turn on ~y~advertising'
 }
